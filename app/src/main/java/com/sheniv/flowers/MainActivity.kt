bbb7369100f23e45e730baae7bbf4c40b6689711
Nikita_Shekov <shekovnikita8@gmail.com>
2023-05-05 12:01:20 +0200

    All works

--- app/src/main/java/com/sheniv/flowers/MainActivity.kt
+++ app/src/main/java/com/sheniv/flowers/MainActivity.kt
@@ -9,11 +9,18 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.sheniv.flowers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    private var appUpdateManager: AppUpdateManager? = null
+    private val REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +28,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //supportActionBar?.show()
+
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        checkUpdate()
+    }
+
+    private fun checkUpdate() {
+        appUpdateManager?.appUpdateInfo?.addOnSuccessListener { updateInfo ->
+            if (updateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && updateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                appUpdateManager?.startUpdateFlowForResult(
+                    updateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    REQUEST_CODE
+                )
+
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        inProgressUpdate()
+    }
+
+    private fun inProgressUpdate() {
+        appUpdateManager?.appUpdateInfo?.addOnSuccessListener { updateInfo ->
+            if (updateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                appUpdateManager?.startUpdateFlowForResult(
+                    updateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    REQUEST_CODE
+                )
+            }
+        }
     }
 }
